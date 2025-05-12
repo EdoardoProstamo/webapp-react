7b972b69b855c2828511489cb0a03ca04e7f8948
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-05-12 19:04:44 +0200

    feat: rotte

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
       Movies
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<div>Homepage</div>} />
+          <Route path='/movies/:id' element={<div>Movie review</div>} />
+          <Route path='*' element={<div>Errore! Elemento non trovato.</div>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 };

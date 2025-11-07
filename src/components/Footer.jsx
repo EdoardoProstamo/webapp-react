55442a6e69ad65c1fefdad841c82e077d1e57b4d
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-11-07 16:39:28 +0100

    add: bootstrap in header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,11 @@
 const Footer = () => {
-
-    return <>
-        <div>Footer della pagina</div>
-    </>
+    return (
+        <footer className="bg-dark text-light py-3 mt-4">
+            <div className="container text-center">
+                <p className="mb-0">© 2025 IMDb(oolean). Tutti i diritti riservati.</p>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,42 @@
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+            <div className="container">
+                <NavLink className="navbar-brand" to="/">
+                    IMDb(oolean)
+                </NavLink>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
 
-    return <>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/">
+                                Home
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/movies/add">
+                                Aggiungi Film
+                            </NavLink>
+                        </li>
 
-        <nav>
-            <ul>
-                <li>
-                    <NavLink to="/">IMDb(oolean)</NavLink>
-                </li>
-            </ul>
+                    </ul>
+                </div>
+            </div>
         </nav>
-    </>
+    );
 };
 
 export default Header;
